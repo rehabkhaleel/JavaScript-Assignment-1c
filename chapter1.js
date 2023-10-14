f041f0ad1b0d98514842272f82ebe1b4255fa7b5
Rehab <140162802+rehabkhaleel@users.noreply.github.com>
2023-10-14 18:54:20 +0500

    Add files via upload

---
+++ chapter1.js
@@ -0,0 +1,7 @@
+// console.log("Rehab");
+
+// let a = 10; //setting value of a equals to 10
+// console.log(a); // printing a
+// /*
+// this will print the value of a which is currently set to 10
+// */
